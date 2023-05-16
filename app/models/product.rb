759a34590afc4e525da3a98988701dc84add8c8d
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-05-16 21:35:50 +0300

    add Product model, controller, validations, simple views

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ApplicationRecord
+  before_create :round_price
+
+  validates :name, :description, presence: true
+  validates :price, :balance, numericality: {greater_than: 0}
+
+  private
+
+  def round_price
+    self.price = self.price.round(2)
+  end
+end
