e2e20a5d4189776f2c52f0d74b6cc7dba56e49b5
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-05-17 22:05:11 +0300

    fix problem with deleting order, validation need more updates

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,14 @@ class Order < ApplicationRecord
   has_many :product_orders, dependent: :destroy
   has_many :products, through: :product_orders
 
-  validates :user_id, presence: true
+  # before_destroy :customer_validation
+  #
+  # private
+  #
+  # def customer_validation
+  #   if firstname == 'null_value'
+  #     errors.add(:firstname, "cannot be 'null_value'")
+  #     throw(:abort)
+  #   end
+  # end
 end
