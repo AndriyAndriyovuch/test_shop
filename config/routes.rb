6cc570bd2617ff5734baf3d8a8a55e735d4ecd70
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-05-17 16:44:14 +0300

    add ability to delete product from cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'buy', to: 'orders#create'
   get 'cart', to: 'orders#index'
   delete 'pay/:id', to: 'orders#destroy', as: 'pay_order'
+  delete 'delete/product/:id', to: 'product_orders#destroy', as: 'delete_product'
 
   root 'main#index'
 end
