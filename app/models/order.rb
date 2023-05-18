e08b6902b01d1f10baf93e67e079fcbf7c56843c
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-05-18 12:23:53 +0300

    add validation when order is done

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,5 @@ class Order < ApplicationRecord
   has_many :product_orders, dependent: :destroy
   has_many :products, through: :product_orders
 
-  # before_destroy :customer_validation
-  #
-  # private
-  #
-  # def customer_validation
-  #   if firstname == 'null_value'
-  #     errors.add(:firstname, "cannot be 'null_value'")
-  #     throw(:abort)
-  #   end
-  # end
+  validates :firstname, :lastname, :address, :phone, presence: true, on: :update
 end
